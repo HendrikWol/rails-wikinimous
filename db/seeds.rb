c9a6060f00eaa39da3b2b85c30fd78388fcaeeaa
Hendrik Wolleswinkel <hendrikwolleswinkel@me.com>
2017-09-05 16:29:17 +0200

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+puts "Destroying articles"
+Article.destroy_all
+
+puts "Creating articles"
+10.times do
+  post = Article.new(
+    title: Faker::Commerce.product_name,
+    content: Faker::ChuckNorris.fact,
+    # votes: (0..1000).to_a.sample
+  )
+  puts "Created #{post.title}"
+  post.save
+end
